d3ce08be11e9c9ebcd4d952f0dfdae4fbf1c0e21
shkomg <serhiy.khvashchuk@gmail.com>
2015-11-20 14:03:18 +0200

    phantomjs2

--- dfischer:phantomjs.js
+++ dfischer:phantomjs.js
@@ -1,2 +1,2 @@
-var phantomjs = Npm.require('phantomjs'), path = Npm.require('path'); // include
+var phantomjs = Npm.require('phantomjs2'), path = Npm.require('path'); // include
 process.env.PATH += ':' + path.dirname(phantomjs.path);
